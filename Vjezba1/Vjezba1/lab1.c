fb7acbb02e4e2083ac1583ab176298edbaecfe42
jdumic00 <jdumic00@fesb.hr>
2023-10-11 20:03:13 +0200

    wrote hello worl

--- Vjezba1/Vjezba1/lab1.c
+++ Vjezba1/Vjezba1/lab1.c
@@ -1,9 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+#define FILE_ERROR_OPEN (-1)
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} student;
+
+int readNoRows()
+{
+	int counter = 0;
+	FILE* filepointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+
+	filepointer = fopen("studenti.txt", "r");
+	if (!filepointer) {
+		printf("file is not open");
+		return FILE_ERROR_OPEN;
+	}
+
+	while (!feof(filepointer))
+	{
+		fgets(buffer, MAX_LINE, filepointer);
+		counter++;
+	}
+	fclose(filepointer);
+	return counter;
+}
 
 int main()
 {
 
-	printf("Hello World");
+	int noRows = 0;
+
+	noRows = (readNoRows);
+
+	printf("%d", noRows);
 
 	return 0;
 }
